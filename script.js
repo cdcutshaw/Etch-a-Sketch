a26cc7f87aaec95449e1db62685d08deac056ed8
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-06-13 16:16:11 -0600

    add effect to change color of grid squares when mouse hovers over them

--- script.js
+++ script.js
@@ -2,21 +2,31 @@ const container = document.querySelector("#container");
 
 function makeGrid() 
 {
-    let rowNumber = 16;
-    let colNumber = 16;
-
-    for (let i = 0; i < rowNumber; i++)
+    let gridSize = 16;
+  
+    for (let i = 0; i < gridSize; i++)
     {
-        let rowItem = document.createElement("div");
+        const rowItem = document.createElement("div");
         rowItem.className = "rowItem";
 
-        for(let j = 0; j < colNumber; j++)
+        for(let j = 0; j < gridSize; j++)
             {
-                let colItem = document.createElement("div");
+                const colItem = document.createElement("div");
                 colItem.classList.add("colItem");
                 rowItem.appendChild(colItem);
-            }
-            container.appendChild(rowItem);
+                container.appendChild(rowItem);
+
+                colItem.addEventListener("mouseover", () => 
+                {
+                    colItem.style.backgroundColor = "black"; 
+                })
+            }   
     }
 }
 makeGrid();
+
+
+function updateGrid () {
+    
+}
+
