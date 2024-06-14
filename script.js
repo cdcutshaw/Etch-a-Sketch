144c1fc00a01a6b7d1bd42c4a5f1c6b1617b78a8
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-06-14 16:17:00 -0600

    add function to clear grid and make new grid sized according to prompt input

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector("#container");
 
-function makeGrid() 
+function makeGrid(gridSizeNumber) 
 {
-    let gridSize = 16;
+    let gridSize = gridSizeNumber;
   
     for (let i = 0; i < gridSize; i++)
     {
@@ -23,19 +23,38 @@ function makeGrid()
             }   
     }
 }
-makeGrid();
+
+function deleteGrid()
+{
+    const rows = document.querySelectorAll(".rowItem");
+    for(let rowItem of rows) {
+    container.removeChild(rowItem);
+    }
+
+}
+
 
 //function, promptUpdateGrid, that prompts user for new gridSize when UpdateBtn is clicked => newGridSize
 //function, updateGrid, that changes gridSize to newGridSize
 
-
+makeGrid(16);
 
 const updateBtn = document.querySelector("#updateBtn");
 updateBtn.addEventListener("click", () => 
 {
-    let newGridSize = prompt ("Enter a number between 1-100:");
+    let newGridSize = prompt ("Set grid scale. Enter a number between 1-100:");
     console.log(newGridSize)
-    
+    let gridSize = newGridSize;
+        if (isNaN(newGridSize) || newGridSize < 1 || newGridSize > 100) 
+            {
+                alert ("Invalid Entry. Please enter a number between 1-100");
+            }
+        else    
+            {
+            deleteGrid();
+
+            makeGrid(gridSize);
+            }
 })
     
 
